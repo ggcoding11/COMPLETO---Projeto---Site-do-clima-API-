28a7ba5edab76ed8fb97200ff07cebde0a754568
Gustavo <gustavogomes1108@gmail.com>
2025-05-01 15:21:12 -0300

    adicao-modo-escuro

---
+++ Projeto 8 - Site de clima (API)/script.js
@@ -0,0 +1,95 @@
+let modoEscuro = false
+
+document.getElementById("botao-modo-escuro").addEventListener("click", () => {
+    let iconeModoEscuro = document.getElementById("icone-modo-escuro")
+    let navbar = document.querySelector(".navbar")
+    let tituloNavbar = document.querySelector(".navbar-brand")
+    let cardsInfo = document.querySelectorAll(".info")
+    let body = document.getElementsByTagName("body")[0]
+    let cardContainer = document.querySelector(".card-container")
+
+    if (modoEscuro == false) {
+        modoEscuro = true
+        iconeModoEscuro.className = "bi bi-moon-fill"
+        navbar.className += " dark-mode"
+        tituloNavbar.className += " dark-mode"
+        cardsInfo.forEach(card => {
+            card.className += " dark-mode"
+        })
+
+        body.style.backgroundImage = "url('/Projeto 8 - Site de clima (API)/img/img-blur-noite.png')";
+        cardContainer.className = document.querySelector(".card-container").className.replace("white", "secondary")
+    } else {
+        modoEscuro = false
+        iconeModoEscuro.className = "bi bi-moon"
+        navbar.className = document.querySelector(".navbar").className.replace("dark-mode", "")
+        tituloNavbar.className = document.querySelector(".navbar-brand").className.replace("dark-mode", "")
+
+        cardsInfo.forEach(card => {
+            card.className = card.className.replace(" dark-mode", "")
+        })
+
+        body.style.backgroundImage = "url('/Projeto 8 - Site de clima (API)/img/img-blur.png')";
+        cardContainer.className = document.querySelector(".card-container").className.replace("secondary", "white")
+    }
+})
+
+let localAtual = "Maringá"
+const chaveAPI = "290fc79807794f89812162023252404"
+
+document.getElementById("buscar-local").addEventListener("click", function () {
+    let localPesquisado = document.getElementById("local-pesquisado").value
+
+    carregarInformacoes(localPesquisado)
+})
+
+function carregarInformacoes(local) {
+    //Mostra as informações do clima do local respectivo
+
+    fetch(`https://api.weatherapi.com/v1/current.json?key=${chaveAPI}&q=${local}`)
+        .then(response => response.json())
+        .then(data => {
+            let condicaoAtual = data.current.condition.text
+
+            document.getElementById("condicao-clima").innerText = condicaoAtual
+
+            let nomeImg = condicaoAtual.toLowerCase()
+            nomeImg = nomeImg.replace(/ /g, "-")
+            nomeImg += ".png"
+
+            let enderecoImg = "/Projeto 8 - Site de clima (API)/img/icones-climas/" + nomeImg
+
+            let iconeClima = document.getElementById("icone-clima")
+            iconeClima.setAttribute("src", enderecoImg)
+            iconeClima.setAttribute("alt", condicaoAtual)
+
+            document.getElementById("nome-local").innerText = (data.location.name).toUpperCase()
+
+            let pais = data.location.country
+
+            document.getElementById("temp-atual").innerText = data.current.temp_c + "°C"
+
+            fetch(`https://api.weatherapi.com/v1/forecast.json?key=${chaveAPI}&q=${local}&days=1`)
+                .then(responseFcast => responseFcast.json())
+                .then(dataFcast => {
+                    document.getElementById("temp-minima").innerText = dataFcast.forecast.forecastday[0].day.mintemp_c + "°C"
+                    document.getElementById("temp-maxima").innerText = dataFcast.forecast.forecastday[0].day.maxtemp_c + "°C"
+                    document.getElementById("umidade-relativa").innerText = dataFcast.forecast.forecastday[0].day.avghumidity + "%"
+                })
+
+            fetch(`https://restcountries.com/v3.1/name/${pais.toLowerCase()}`)
+                .then(responseFlag => responseFlag.json())
+                .then(dataFlag => {
+                    let bandeiraLocal = document.getElementById("bandeira-local")
+                    bandeiraLocal.setAttribute("src", dataFlag[0].flags.png)
+                    bandeiraLocal.setAttribute("alt", pais)
+                })
+        })
+
+        .catch(erro => {
+            console.error(erro)
+        })
+}
+
+carregarInformacoes(localAtual)
+//É o local padrão que mostra quando eu abro o site!
